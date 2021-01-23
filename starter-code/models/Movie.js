35f31a0b4c9e4a6d37467540da465ce483ebb8c3
UmaCouve <lourencokramerhorta@gmail.com>
2021-01-23 15:34:25 +0000

    done

---
+++ starter-code/models/Movie.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const movieSchema = new Schema(
+  {
+    title: String,
+    genre: String,
+    plot: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = model("Movie", movieSchema);
